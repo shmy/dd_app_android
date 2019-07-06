159b24c8d4f2bb29cdad0b8a2f67799000415b5a
shmy <pandahao123>
2019-07-06 11:17:04 +0800

    添加缓存清理

--- app/src/main/java/tech/shmy/dd_app/activity/AboutActivity.java
+++ app/src/main/java/tech/shmy/dd_app/activity/AboutActivity.java
@@ -7,11 +7,14 @@ import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import com.allen.library.SuperTextView;
 import com.kaopiz.kprogresshud.KProgressHUD;
 
 import org.lzh.framework.updatepluginlib.UpdateBuilder;
 import org.lzh.framework.updatepluginlib.base.CheckCallback;
 import org.lzh.framework.updatepluginlib.model.Update;
+
+import java.text.DecimalFormat;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -19,12 +22,15 @@ import butterknife.OnClick;
 import es.dmoral.toasty.Toasty;
 import tech.shmy.dd_app.R;
 import tech.shmy.dd_app.defs.BaseActivity;
+import tech.shmy.dd_app.util.Util;
 
 import static tech.shmy.dd_app.defs.Env.WEBVIEW_BASE_URL;
 
 public class AboutActivity extends BaseActivity {
     @BindView(R.id.version)
     public TextView versionTextView;
+    @BindView(R.id.clear_cache)
+    public SuperTextView clearCacheView;
     private KProgressHUD kProgressHUD;
 
     @Override
@@ -40,6 +46,7 @@ public class AboutActivity extends BaseActivity {
                 .setCancellable(false)
                 .setDimAmount(0.5f);
         versionTextView.setText(getVersionNameAndCode(this));
+        refreshCacheSize();
     }
 
     private String getVersionNameAndCode(Context context) {
@@ -92,29 +99,87 @@ public class AboutActivity extends BaseActivity {
             }
         }).check();
     }
+
     @OnClick(R.id.group)
     void onGruopClick() {
         Intent intent = new Intent(this, WebViewActivity.class);
         intent.putExtra("url", WEBVIEW_BASE_URL + "/join_group");
         pushActivity(intent);
     }
+
     @OnClick(R.id.help_center)
     void onHelpCenterClick() {
         Intent intent = new Intent(this, WebViewActivity.class);
         intent.putExtra("url", WEBVIEW_BASE_URL + "/help_center");
         pushActivity(intent);
     }
+
     @OnClick(R.id.feedback)
     void onFeedBackClick() {
         Intent intent = new Intent(this, WebViewActivity.class);
         intent.putExtra("url", WEBVIEW_BASE_URL + "/feedback");
         pushActivity(intent);
     }
+
     @OnClick(R.id.complaint)
     void onComplaintClick() {
         Intent intent = new Intent(this, WebViewActivity.class);
         intent.putExtra("url", WEBVIEW_BASE_URL + "/complaint");
         pushActivity(intent);
+    }
+
+    @OnClick(R.id.clear_cache)
+    void onClearCache() {
+        kProgressHUD = KProgressHUD.create(AboutActivity.this).setStyle(KProgressHUD.Style.SPIN_INDETERMINATE)
+                .setLabel("请稍后")
+                .setDetailsLabel("正在清理缓存...")
+                .setCancellable(true)
+                .setAnimationSpeed(2)
+                .setCancellable(false)
+                .setDimAmount(0.5f);
+        String[] keys = Util.mmkv.allKeys();
+        for (String key : keys) {
+            if (key.equals("token")) {
+                continue;
+            }
+            Util.mmkv.remove(key);
+        }
+        refreshCacheSize();
+        kProgressHUD.dismiss();
+    }
+
+    private void refreshCacheSize() {
+        String[] keys = Util.mmkv.allKeys();
+        long size = 0;
+        for (String key : keys) {
+            if (key.equals("token")) {
+                continue;
+            }
+            size += Util.mmkv.getValueSize(key);
+        }
+        clearCacheView.setRightString(getSizeText(size));
+
+    }
+    private String getSizeText(long size) {
+        //获取到的size为：1705230
+        int GB = 1024 * 1024 * 1024;//定义GB的计算常量
+        int MB = 1024 * 1024;//定义MB的计算常量
+        int KB = 1024;//定义KB的计算常量
+        DecimalFormat df = new DecimalFormat("0.00");//格式化小数
+        String resultSize = "";
+        if (size / GB >= 1) {
+            //如果当前Byte的值大于等于1GB
+            resultSize = df.format(size / (float) GB) + "GB";
+        } else if (size / MB >= 1) {
+            //如果当前Byte的值大于等于1MB
+            resultSize = df.format(size / (float) MB) + "MB";
+        } else if (size / KB >= 1) {
+            //如果当前Byte的值大于等于1KB
+            resultSize = df.format(size / (float) KB) + "KB";
+        } else {
+            resultSize = size + "B";
+        }
+        return resultSize;
     }
 
 }
